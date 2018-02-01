b54b6a5b7dec3614ac8c131378055d0de147a3bd
xav <xavierartot@gmail.com>
2018-02-01 15:03:10 -0600

    add a composant for the loop

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Axios from 'axios';
 
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      isLoading: false,
-      prom : []
+      isLoading  : false,
+      reqAlluser : true,
+      req30Days  : false,
+      isHover    : '',
+      prom       : []
     }
   }
   componentDidMount() {
@@ -19,46 +21,96 @@ class App extends Component {
     Axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
       .then( res => {
         return this.setState({ 
-          isLoading: false,
-          prom: res.data 
+          isLoading  : false,
+          reqAlluser : true,
+          req30Days  : false,
+          prom       : res.data
         })
       })
       .catch(function (error) {
         console.log(error);
       });
   }
+  handlePast30Days = (event) => {
+    //https://fcctop100.herokuapp.com/api/fccusers/top/recent
+    Axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then( res => {
+        return this.setState({ 
+          isLoading  : false,
+          reqAlluser : false,
+          req30Days  : true,
+          prom       : res.data
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log(event);
+  }
+  handleAllTime = (event) => {
+    //To get the top 100 campers of all time: 
+    Axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+      .then( res => {
+        return this.setState({ 
+          isLoading  : false,
+          reqAlluser : true,
+          req30Days  : false,
+          prom       : res.data
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  eventHover = (e) => {
+    this.setState({
+      isHover: !this.state.isHover
+    });
+  }
   //vue
   render() {
-    const { prom, isLoading } = this.state;
+    const { prom, reqAlluser, req30Days, isLoading } = this.state;
     let loopAllData = prom.map( (element,i) => {
-     return  <tr className="table-info">
-        <th scope="row">{i+1}</th>
-        <td>
-          <span className="mr-3">
-            <img src={element.img} alt={i} className='rounded-circle' />
-          </span>
-          {element.username}
-        </td>
-        <td>{element.recent}</td>
-        <td>{element.alltime}</td>
-      </tr>
+      return <LoopAllData 
+        key={i}
+        index={i}
+        element={element}
+      />
     });
-
+    
     if (isLoading) {
       return <p>Loading ...</p>;
     }
+    let reqAll = req30Days ? 'border p-2 border-info': ''
+    let req30  = reqAlluser ? 'border p-2 border-warning': ''
+    const thBtn = this.state.isHover ? 'border-danger': ''
     return (
       <div className="container">
         <div className="row">
           <h3>the cream of the cream</h3>
         </div>
-        <table className="table">
+        <table className="table table-responsive">
           <thead>
             <tr>
-              <th>Rank</th>
-              <th className='rounded-circle'>Camper Name</th>
-              <th><a>Points in past 30 days</a></th>
-              <th><a>All time points</a></th>
+              <th className='text-center'>Rank</th>
+              <th className='text-left pl-9'>Camper Name</th>
+              <th 
+                className='text-center' >
+                <a 
+                  onClick={this.handlePast30Days} 
+                  onMouseEnter={this.eventHover} 
+                  onMouseLeave={this.eventHover}
+                  className={ `${req30} th-30 ${thBtn}` }>
+                  Points in past 30 days
+                </a>
+              </th>
+              <th className='text-center'>
+                <a 
+                  onClick={this.handleAllTime} 
+                  className={ `${reqAll} th-all-time` }>
+                  All time points
+                </a>
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -69,5 +121,19 @@ class App extends Component {
     );
   }
 }
-
+const LoopAllData = ({element, index }) =>  {
+  return ( 
+    <tr className="text-center">
+      <td className="text-center font-weight-bold">{index+1}</td>
+      <td className='text-left pl-5 no-img'>
+        <span className="mr-3">
+          <img src={element.img} alt={index} className='rounded-circle' />
+        </span>
+        {element.username}
+      </td>
+      <td className='text-center'>{element.recent}</td>
+      <td className='text-center'>{element.alltime}</td>
+    </tr>
+  )
+}
 export default App;
